f680f5d956ac37f2c64db9077bfc172fd827bffb
KF6S <43057968+KF6S@users.noreply.github.com>
2020-03-10 22:56:42 -0700

    Update cpyfiles.sh

--- cpyfiles.sh
+++ cpyfiles.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#########################################################
-#  Nextion Support for Nextion screen. Used to copy     #
-#  files from Nextion.Images to their proper location   #
-#                                                       #
-#  KF6S                                     03-10-2020  #
-#########################################################
+#############################################################
+#  Nextion Support for Nextion screen. Used to copy         #
+#  files from correct screen model repository to the        #
+#  /usr/local/etc/Nextion_Support directory.                #
+#                                                           #
+#  KF6S                                         03-10-2020  #
+#############################################################
 
 sudo mount -o remount,rw /
 sudo mkdir /usr/local/etc/Nextion_Support

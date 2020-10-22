0cfa5a0771ef9c88cfb6a0df024e42d602bfdb3f
KF6S <43057968+KF6S@users.noreply.github.com>
2020-10-21 17:01:05 -0700

    Create getDMRnetwork.sh

---
+++ getDMRnetwork.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+############################################################
+#  Get DMR Network from /etc/mmdvmhost                     #
+#                                                          #
+#                                                          #
+#                                                          #
+#                                                          #
+#  KF6S                                        10-21-2020  #
+############################################################
+set -o errexit
+set -o pipefail
+
+sudo cat /etc/mmdvmhost | grep "\[DMR Network\]" -A 2 | grep "Address=" | cut -b 9-59

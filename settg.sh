b22229877f97b890ca53b6bf10a2af683efd2b0a
KF6S <43057968+KF6S@users.noreply.github.com>
2020-10-25 16:39:32 -0700

    Create settg.sh

---
+++ settg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#########################################################
+#  Nextion Support for Nextion screen. Used to dump     #
+#  current TG and change to TG4000                      #
+#                                                       #
+#  KF6S                                     10-25-2020  #
+#########################################################
+
+# Use TS2 (zero based code=1)
+TS="1"
+if [ -z "$1" ]; then
+# If TG is not passed then default to 31665
+TG=31665
+else
+TG=$1
+fi
+
+ID=`sudo cat /etc/mmdvmhost | grep "\[DMR\]" -A 14 | grep "Id=" | cut -b 4-14`
+curl -s http://tgif.network:5040/api/sessions/update/$ID/$TS/$TG
+## To check arquments being passed to command take off the ## in front of echo below
+echo curl -s http://tgif.network:5040/api/sessions/update/$ID/$TS/$TG

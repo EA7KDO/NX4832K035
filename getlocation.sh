2975c5b8ed2cab3b3906f2ae5ca21aa370bbc569
EA7KDO <42783056+EA7KDO@users.noreply.github.com>
2021-06-08 05:31:16 +0200

    Add files via upload

---
+++ getlocation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+############################################################
+#  Get  Location from /etc/mmdvmhost                       #
+#                                                          #
+#  Pass 1 to the script                                                        #
+#  Returns the Location from mmdvmhost                     #
+#                                                          #
+#  EA7KDO                                      06-08-2021  #
+############################################################
+set -o errexit
+set -o pipefail
+
+if [ "$1" = 1 ]; then  sudo cat /etc/mmdvmhost | grep "\[Info\]" -A 7 | grep "Location=" | cut -b 10-30
+fi
+

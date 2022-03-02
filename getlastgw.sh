3d7498091dd69465b2cbbf05ecb3ab07ea6dc1f5
Philip Thompson <43919012+VE3RD@users.noreply.github.com>
2022-03-02 17:05:33 -0500

    updated for P25

--- getlastgw.sh
+++ getlastgw.sh
@@ -8,48 +8,76 @@
 set -o errexit
 set -o pipefail
 
-function getstring
-{
-# Step 1 - Get newest dmrgateway log file
+if [ -n "$(ls -A /var/log/pi-star/MM* 2>/dev/null)" ]; then   
+  f2=$(ls -1 /var/log/pi-star/MMDVM* | tail -n 1)
+  #echo "$f2"
+  line=$(sudo cat "$f2" | grep transmission | tail -n 1)
+  #echo "$line"
+  nMode=$(echo "$line" | cut -d " " -f 4 | cut -d "," -f 1)
+#  echo "$nMode"
+  result="Starting"
 
-	f1=$(ls -art /var/log/pi-star/DMRG* | tail -n1)
-#	echo "File: $f1"
+  if [[ ! "$nMode" =~ ^(DMR|P25|YSF|NXDN)$ ]]; then
+	echo "NA|NA|NA"
+	exit
+    
+  fi
 
-# Sterp 2 - Get the last line that contains 'Name='
-	line=$(grep Name= $f1 | tail -n 1)
-#	echo "1:$line"
-
-# Step 3 - Get Param 4 to Determine RF or Network
-	type=$(echo "$line" | cut -d' ' -f4)
-#	echo "type=$type"
-
-
-# Step 4 Get Network Name and Number
-
-	if [ "$type" = "RF" ]; then
-		tt1=$(echo "$line" | cut -d' ' -f6 | cut -d'=' -f2)     # Net Name
-		tt2=$(echo "$line" | cut -d' ' -f7 | cut -d'=' -f2)     # Net Number
-		echo "GW R Net $tt2 $tt1" 
-		
+#echo "Mode = $nMode"
+  
+  if [ "$nMode" == "DMR" ]; then
+	Addr=$(sudo sed -nr "/^\[DMR Network\]/ { :l /^Address[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/mmdvmhost)
+        if [ $Addr = "127.0.0.1" ]; then
+			f1=$(sudo ls -x /var/log/pi-star/DMRGateway* | tail -n1)
+                	GW="ON"
+                	NetType=$(sudo tail -n1 "$f1" | cut -d " " -f 4)
+			NetNum=$(sudo tail -n1 "$f1" | cut -d " " -f 6)
+			NName=$(sudo sed -nr "/^\[DMR Network "${NetNum##*( )}"\]/ { :l /^Name[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/dmrgateway)
+			NName=$(echo "$NName" | cut -d "_" -f1 |  tr '[:lower:]' '[:upper:]')
+#			NName=$(echo "$NName" |  tr '[:lower:]' '[:upper:]')
+   			result=$(echo "DMR|$NName|GW:${NetNum##*( )}")
+	else
+        
+                	GW="OFF"
+			ms1=$(sudo sed -n '/^[^#]*'"$Addr"'/p' /usr/local/etc/DMR_Hosts.txt | tail -n 1 | sed -E "s/[[:space:]]+/|/g") 
+#			ms=$(echo "$ms1" | cut -d '|' -f 1 | cut -d "_" -f 1)
+			ms=$(echo "$ms1" | cut -d '|' -f 1 )
+                	result=$(echo "DMR|$ms|NA")
 	fi
+#	echo "Mode=DMR"
 
-	if [ "$type" = "Network" ]; then
-		t1=$(echo "$line" | cut -d' ' -f8 | cut -d'=' -f2)      #Net Name
-		t2=$(echo "$line" | cut -d' ' -f5)	#Net Number
-		echo "GW N Net $t2 $t1"
 
-	fi
+    elif [ "$nMode" == "P25" ]; then
+#		echo "Mode=P25"
+  		line=$(sudo cat "$f2" | grep 'end of transmission' | tail -n 1)
+#echo "$line"
+ 		 tg=$(echo "$line" | cut -d " " -f 13 | cut -d "," -f1)
+#echo "$tg"
+		server=$(grep "$tg" /usr/local/etc/P25HostsLocal.txt |  tr '\t' ' ' | cut -d " " -f2 | cut -d "." -f1 | tr '[:lower:]' '[:upper:]')
+		if [ -z "$server" ]; then
+			server=$(grep "$tg" /usr/local/etc/P25Hosts.txt |  tr '\t' ' ' | cut -d " " -f2 | cut -d "." -f1 | tr '[:lower:]' '[:upper:]')
+		fi
+		result=$(echo "P25|$server|NA")
+	
 
-}
+     elif [ "$nMode" == "YSF" ]; then
+		tg=$(sed -nr "/^\[Network\]/ { :l /^Startup[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/ysfgateway)
+		server=$(grep "$tg" /usr/local/etc/YSFHosts.txt |  tr '\t' ' ' | cut -d ";" -f3 | cut -d "." -f1 | tr '[:lower:]' '[:upper:]')
+		result=$(echo "YSF|$server|NA")
+	
 
-Addr=$(sed -nr "/^\[DMR Network\]/ { :l /^Address[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/mmdvmhost)
+     elif [ "$nMode" == "NXDN" ]; then
+		echo "Mode=NXDN"
+		tg=$(sed -nr "/^\[Network\]/ { :l /^Static[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/nxdngateway)
+		echo "tg-$tg"
+		server=$(grep "$tg" /usr/local/etc/NXDNHosts.txt |  tr '\t' ' ' | cut -d " " -f2 | cut -d "." -f1 | tr '[:lower:]' '[:upper:]')
+		result=$(echo "NXDN|$tg|NA")
+      fi
 
-#Check to see if we are running the DMRGatewy
-if [ $Addr = "127.0.0.1" ]; then
- 	GW="ON"
-	getstring
-else
-   	# We are not Running the Gateway - Do Nothing
-	GW="OFF"
+	echo "$result"
+
+else   
+    echo "StartUp"
+	exit
 fi
 
